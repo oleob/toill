d91535fb0a53af1b8430db65325f33abc5cbe267
Torbjørn Viem Ness <tbness@gmail.com>
2017-02-15 14:38:05 +0100

    Created JS for DB initialization

---
+++ db_include.js
@@ -0,0 +1,24 @@
+// Include this file to use DB connectivity, queries can be executed on the 'pool' object
+
+var pg = require('pg').native;
+
+// create a config to configure both pooling behavior
+// and client options
+// note: all config is optional and the environment variables
+// will be read if the config is not present
+var config = {
+      user: 'foo', //env var: PGUSER
+      database: 'my_db', //env var: PGDATABASE
+      password: 'secret', //env var: PGPASSWORD
+      host: 'localhost', // Server hosting the postgres database
+      port: 5432, //env var: PGPORT
+      max: 10, // max number of clients in the pool
+      idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
+};
+
+
+//this initializes a connection pool
+//it will keep idle connections open for a 30 seconds
+//and set a limit of maximum 10 idle clients
+var pool = new pg.Pool(config);
+
